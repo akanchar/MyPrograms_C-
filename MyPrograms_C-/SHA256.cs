d60418ac1d4fa95377e4bc6b694eda477bf7327d
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-04 23:58:25 -0600

    MOD: Minor changes

---
+++ MyPrograms_C-/SHA256.cs
@@ -0,0 +1,14 @@
+﻿namespace MyPrograms_C_
+{
+  class SHA256
+  {
+    const int sha_algo_number = 256;
+    const int initialPrimes = 8;
+    const int secondaryPrimes = 64;
+    const string inputString = "abc";
+    static void Main(string[] args)
+    {
+
+    }
+  }
+}
