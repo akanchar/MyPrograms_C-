96f7d8738cdd14fd4b8780b83c847f75b48e6b87
Abhilash Kancharla <kancharla.abhilash@outlook.com>
2018-03-07 20:13:19 -0600

    ADD: Added a main function containing cs file

---
+++ MyPrograms_C-/Home.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyPrograms_C_
+{
+  class Home
+  {
+    public static void Main(String[] args)
+    {
+      BinaryConversion binaryConversion = new BinaryConversion();
+      string str;
+      binaryConversion.stringToBits("aA", 8, out str);
+      Console.WriteLine(str);
+    }
+  }
+}
